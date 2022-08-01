4d346fca40e65273bdf286cbcfb0565285e1b90b
Ramazani Mwemedi <ramazanimwemedi234@gmail.com>
2022-08-01 16:14:38 +0300

    Exercise 1

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-var moneyReieved= 19000
-var message= "19000 🙋‍♂️"
-console.log(message);
-console.log(typeof message);
+// Exercise 1
+// Answer 1
+const logedinUser = "Ramazani";
+console.log(logedinUser);
+
+// Answer 2
+const age = 14;
+console.log(age);
+
+// Answer 3
+if (age < 13) {
+  alert("You are not allowed to use this platform");
+} else{
+    alert("Welcome "+ logedinUser)
+}
