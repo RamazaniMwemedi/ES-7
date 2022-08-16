a02d86e69d9f6a3315a77c7a1d54dca5c66da37c
Ramazani Mwemedi <ramazanimwemedi234@gmail.com>
2022-08-16 16:40:13 +0300

    Fuction in HTML

--- index.js
+++ index.js
@@ -1,54 +1,16 @@
-// Array of 10 students
-var students = [
-  {
-    name: "John",
-    age: 30,
-  },
-  {
-    name: "Jane",
-    age: 31,
-  },
-  {
-    name: "Joe",
-    age: 32,
-  },
-  {
-    name: "Jack",
-    age: 33,
-  },
-  {
-    name: "Jill",
-    age: 34,
-  },
-  {
-    name: "Jana",
-    age: 35,
-  },
-  {
-    name: "Juan",
-    age: 36,
-  },
-  {
-    name: "Zoe",
-    age: 36,
-  },
-  {
-    name: "Mark",
-    age: 23,
-  },
-  {
-    name: "Clark",
+const alertName = () => {
+  alert("Ramazani Mwemedi");
+  confirm("Is your name Ramazani");
+};
 
-    age: 20,
-  },
-  {
-    name: "Jenny",
-    age: 21,
-  },
-];
+const openNewTab = () => {
+  window.open(
+    "/login.html",
+    "_blank",
+    "width=500, height=500 , left=500, top=50"
+  );
+};
 
-// Map and Filter exercises
-// 1. Map the array to an array of objects with the name and age properties
-// 2. Filter the array to only include students older than 30
-// 3. Filter the array to only include students younger than 30
-// 4. Filter the array to only include students whose name is longer than 5 characters
+const closeTab = () => {
+  window.close();
+}
