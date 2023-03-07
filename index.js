687dbda76e5ee0d2c8ae75017107426795b3da14
RamazaniMwemedi <ramazanimwemedi234@gmail.com>
2023-03-07 17:05:22 +0300

    Added the user profile codes

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 let userNames = ["Kofi", "Ama", "Kwame", "Akosua"];
 
-let userObject = {
-  name: "Kofi",
-  age: 25,
-  message: function () {
-    console.log(`Hello ${this.name} you are ${this.age} years old`);
+let users = [
+  {
+    firstName: "Kofi",
+    lastName: "Ama",
+    profilePic:
+      "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=600",
+    age: 20,
   },
-};
-
-const student = {
-  name: "Kofi",
-  math: 90,
-  physics: 80,
-  total: function () {
-    console.log(
-      `Hey ${this.name} your total is grades are ${this.math + this.physics}`
-    );
+  {
+    firstName: "John",
+    lastName: "Doe",
+    profilePic:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
+    age: 20,
   },
-};
-
-student.total();
-
-userObject.message();
+  {
+    firstName: "Mary",
+    lastName: "Smith",
+    profilePic:
+      "https://images.pexels.com/photos/7275385/pexels-photo-7275385.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=252&fit=crop&h=408",
+    age: 24,
+  },
+];
+userNames.map(function (name) {
+  // Add the names inside ul li to the DOM
+  document.querySelector("ul").innerHTML += `<li>${name}</li>`;
+});
+users.map(function (user) {
+  document.getElementById("users").innerHTML += `
+  <div id="user">
+    <div>
+      <img height="100" width="100" src="${user.profilePic}" alt="profile picture" />
+    </div>
+    <div id="user-info">
+      <h3>${user.firstName} ${user.lastName}</h3>
+      <p>${user.age}</p>
+      
+      <button>Follow</button>
+      <button>Message</button>
+      
+    </div>
+    
+  </div>`;
+});
