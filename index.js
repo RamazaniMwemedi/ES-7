0880a7869cdbfdec105745719babf162336f36fa
RamazaniMwemedi <ramazanimwemedi234@gmail.com>
2023-03-07 15:29:01 +0300

    Added images in user div in users main div using map function

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-let userNames = ["Kofi", "Ama", "Kwame", "Akosua"];
-
-let userObject = {
-  name: "Kofi",
-  age: 25,
-  message: function () {
-    console.log(`Hello ${this.name} you are ${this.age} years old`);
+let userNames = ["Kofi", "Ama", "Kwame", "Akosua", "Yaa", "Afua"];
+let users = [
+  {
+    firstName: "Kofi",
+    lastName: "Ama",
+    profilePic:
+      "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=600",
+    age: 20,
   },
-};
-
-const student = {
-  name: "Kofi",
-  math: 90,
-  physics: 80,
-  total: function () {
-    console.log(
-      `Hey ${this.name} your total is grades are ${this.math + this.physics}`
-    );
+  {
+    firstName: "John",
+    lastName: "Doe",
+    profilePic:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
+    age: 20,
   },
-};
-
-student.total();
-
-userObject.message();
+  {
+    firstName: "Mary",
+    lastName: "Smith",
+    profilePic:
+      "https://images.pexels.com/photos/7275385/pexels-photo-7275385.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=252&fit=crop&h=408",
+    age: 24,
+  },
+];
+// Map
+users.map(function (user) {
+  document.getElementById("users").innerHTML += `
+  <div id="user">
+    <img height="100" width="100" src="${user.profilePic}" alt="profile picture" />
+  </div>`;
+});
